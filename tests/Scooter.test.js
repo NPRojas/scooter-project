d5badfe9e7e7448c724eec979487beb7fbe040ce
Nicole Rojas <63315208+NPRojas@users.noreply.github.com>
2024-04-27 15:36:36 -0400

    write essential tests for the scooter class

--- tests/Scooter.test.js
+++ tests/Scooter.test.js
@@ -1,4 +1,5 @@
-const Scooter = require('../src/Scooter')
+const Scooter = require('../src/Scooter');
+const User = require("../src/User");
 
 // typeof scooter === object
 describe('scooter object', () => {
@@ -11,14 +12,43 @@ describe('scooter object', () => {
 // Method tests
 describe('scooter methods', () => {
   // tests here!
+  let Amanda;
+  let scootey;
+
+  beforeEach(() => {
+    Amanda = new User("Mandy2600", "8dippinDots", 19);
+    scootey = new Scooter("Binghamton Station");  
+  })
 
   // rent method
   test("checkout to User if scooter is charged above 20% and not broken", () =>{
-    const scootey = new Scooter("Binghamton Station");
     scootey.charge = 75;
-    scootey.isBroken = false;
+    scootey.rent(Amanda);
+    expect(scootey.user).toBe("Mandy2600");
+    expect(scootey.station).toBe(null);
   })
+
+  test("throw an error due to uncharged scooter", () => {
+    scootey.charge = 15;
+    expect(() => {
+      scootey.rent(Amanda)
+    }).toThrow("This scooter needs charge. Please select another one from dock.");
+  })
+
+  test("throw and error due to damanged scooter", () => {
+    scootey.isBroken = true;
+    expect(() => {
+      scootey.rent(Amanda)
+    }).toThrow("This scooter is broken. Please select another one from the dock.");
+  })
+  
   // dock method
+  test("return scooter to station and clear user", () => {
+    scootey.rent(Amanda);
+    scootey.dock("Grayson Sqaure");
+    expect(scootey.user).toBe(null);
+    expect(scootey.station).toBe("Greyson Sqaure");
+  })
 
   // requestRepair method
 
